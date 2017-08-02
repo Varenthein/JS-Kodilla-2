a904bbf9cdb572dd98a26196bda1f760fb2f3309
Your Name <you@example.com>
2017-08-02 18:59:58 +0200

    Zadanie 9_1

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
-function triangleArea(a, h) {
+var triangle1Area = getTriangleArea(10, 15);
+var triangle2Area = getTriangleArea(-4, 15);
+var triangle3Area = getTriangleArea(10, 6);
+
+function getTriangleArea(a, h) {
     if (a < 0 || h < 0) {
         return 'nieprawidlowe dane';
-    } else {
-        return a * h / 2;
     }
+	return a * h / 2;
 }
 
-console.log('Wynik = ' + triangleArea(-4, 5));
+console.log('Wynik = ' + triangle1Area);
+console.log('Wynik = ' + triangle2Area);
+console.log('Wynik = ' + triangle3Area);
